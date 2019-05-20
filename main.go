6907a91d010684ad6fb7afa2a47e34c0a9716afb
Tharindu Wickramasinghe <jhtw99@gmail.com>
2019-05-20 18:04:30 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Post struct {
+	Id      int
+	Title   string
+	Body    string
+	Created string
+	User    int
+}
+
+type User struct {
+	Id      int
+	Name    string
+	Email   string
+	Created string
+}
+
+type Company struct {
+	Id       int
+	Name     string
+	Location string
+}
+
+//get all posts
+func allPosts(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM posts ORDER BY created_at DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+	post := Post{}
+	posts := []Post{}
+	for selDB.Next() {
+		var id, user int
+		var title, body, created_at string
+		err = selDB.Scan(&id, &title, &body, &user, &created_at)
+		if err != nil {
+			panic(err.Error())
+		}
+		post.Id = id
+		post.Title = title
+		post.Body = body
+		post.User = user
+		post.Created = created_at
+		posts = append(posts, post)
+	}
+	json.NewEncoder(w).Encode(posts)
+
+	defer db.Close()
+}
+
+//get user data
+func getUser(w http.ResponseWriter, r *http.Request) {
+	userID := mux.Vars(r)["id"]
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM users WHERE id=?", userID)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	user := User{}
+
+	for selDB.Next() {
+		var id int
+		var name, email, created_at string
+		err = selDB.Scan(&id, &name, &email, &created_at)
+		if err != nil {
+			panic(err.Error())
+		}
+		user.Id = id
+		user.Name = name
+		user.Email = email
+		user.Created = created_at
+	}
+	json.NewEncoder(w).Encode(user)
+
+	defer db.Close()
+}
+
+//get all companies
+func allCompanies(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM companies")
+	if err != nil {
+		panic(err.Error())
+	}
+	company := Company{}
+	companies := []Company{}
+	for selDB.Next() {
+		var id int
+		var name, location string
+		err = selDB.Scan(&id, &name, &location)
+		if err != nil {
+			panic(err.Error())
+		}
+		company.Id = id
+		company.Name = name
+		company.Location = location
+		companies = append(companies, company)
+	}
+	json.NewEncoder(w).Encode(companies)
+
+	defer db.Close()
+}
+
+//home page route
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage Endpoint")
+}
+
+//http request handler developed using mux package
+func handleRequests() {
+	testRouter := mux.NewRouter().StrictSlash(true)
+
+	testRouter.HandleFunc("/", homePage)
+	testRouter.HandleFunc("/posts", allPosts).Methods("GET")
+	testRouter.HandleFunc("/companies", allCompanies).Methods("GET")
+	testRouter.HandleFunc("/user/{id}", getUser).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8081", testRouter))
+}
+
+//program starts from here
+func main() {
+	handleRequests()
+}
