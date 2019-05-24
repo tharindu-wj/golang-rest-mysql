7956820f6649f9f8abff865f2b8b36d9dfcde8ad
Tharindu <tharinduw@eight25media.com>
2019-05-24 14:56:36 +0530

    sample database added

--- resources/sample_database.sql
+++ resources/sample_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 23, 2019 at 06:04 PM
+-- Generation Time: May 24, 2019 at 02:55 PM
 -- Server version: 5.7.26-0ubuntu0.18.04.1
 -- PHP Version: 7.2.17-0ubuntu0.18.04.1
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `companies` (
-  `c_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -39,7 +39,7 @@ CREATE TABLE `companies` (
 -- Dumping data for table `companies`
 --
 
-INSERT INTO `companies` (`c_id`, `name`, `location`, `created_at`) VALUES
+INSERT INTO `companies` (`id`, `name`, `location`, `created_at`) VALUES
 (1, 'Company 1', 'Location 1', '2019-05-23 10:48:40'),
 (2, 'Company 2', 'Location 2', '2019-05-23 10:48:40'),
 (3, 'Company 3', 'Location 3', '2019-05-23 10:48:40'),
@@ -52,10 +52,10 @@ INSERT INTO `companies` (`c_id`, `name`, `location`, `created_at`) VALUES
 --
 
 CREATE TABLE `posts` (
-  `p_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `body` longtext NOT NULL,
-  `u_id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -63,11 +63,31 @@ CREATE TABLE `posts` (
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`p_id`, `title`, `body`, `u_id`, `created_at`) VALUES
+INSERT INTO `posts` (`id`, `title`, `body`, `user`, `created_at`) VALUES
 (1, 'What is Lorem Ipsum?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n', 1, '2019-05-20 03:25:21'),
-(2, 'Why do we use it?', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n', 1, '2019-05-20 03:25:21'),
-(3, 'Where does it come from?', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\r\n\r\n', 1, '2019-05-20 03:28:00'),
-(4, 'Where can I get some?', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 1, '2019-05-20 03:28:00');
+(4, 'Where can I get some?', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 1, '2019-05-20 03:28:00'),
+(5, 'test title', 'body', 2, '2019-05-23 13:10:24'),
+(6, 'test title', 'body', 2, '2019-05-23 13:57:09'),
+(7, 'test title', 'body', 2, '2019-05-23 14:03:45'),
+(8, 'test title', 'body', 2, '2019-05-23 14:04:44'),
+(9, 'test title', 'body', 2, '2019-05-23 14:05:15'),
+(10, 'test title', 'body', 2, '2019-05-23 14:17:12'),
+(11, 'test title', 'body', 2, '2019-05-23 14:18:20'),
+(12, 'test title', 'body', 2, '2019-05-23 14:18:58'),
+(13, 'asdf1234', 'body 1234', 1, '2019-05-23 14:32:14'),
+(14, 'asdf1234', 'body 1234', 1, '2019-05-23 14:36:11'),
+(15, '', '', 2, '2019-05-23 14:48:53'),
+(16, '', '', 2, '2019-05-23 14:53:15'),
+(17, '', '', 2, '2019-05-23 14:54:07'),
+(18, 'qwerty', '', 2, '2019-05-23 14:56:37'),
+(19, 'qwerty', '', 1, '2019-05-23 14:59:04'),
+(20, 'qwerty', 'asdf12345', 1, '2019-05-23 14:59:27'),
+(21, 'qwerty', 'asdf12345', 1, '2019-05-23 15:00:18'),
+(22, 'qwerty', 'asdf12345', 1, '2019-05-23 15:13:29'),
+(23, 'qwerty1234', 'asdf12345', 1, '2019-05-23 15:13:59'),
+(24, 'qwerty1234', 'asdf12345', 1, '2019-05-23 15:14:20'),
+(25, 'qwerty1234', 'asdf12345', 1, '2019-05-24 09:03:25'),
+(26, 'Blog Post', 'asdf12345', 1, '2019-05-24 09:04:28');
 
 -- --------------------------------------------------------
 
@@ -76,10 +96,10 @@ INSERT INTO `posts` (`p_id`, `title`, `body`, `u_id`, `created_at`) VALUES
 --
 
 CREATE TABLE `users` (
-  `u_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `c_id` int(11) NOT NULL,
+  `company` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -87,9 +107,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`u_id`, `name`, `email`, `c_id`, `created_at`) VALUES
+INSERT INTO `users` (`id`, `name`, `email`, `company`, `created_at`) VALUES
 (1, 'test user', 'asdf@gmail.com', 1, '2019-05-19 18:30:39'),
-(2, 'Test 2', 'test@gmail.com', 1, '2019-05-20 04:34:06');
+(2, 'Test 2', 'test@gmail.com', 1, '2019-05-20 04:34:06'),
+(4, 'New User', 'asdf3@gmail.com', 1, '2019-05-23 15:19:53');
 
 --
 -- Indexes for dumped tables
@@ -99,22 +120,22 @@ INSERT INTO `users` (`u_id`, `name`, `email`, `c_id`, `created_at`) VALUES
 -- Indexes for table `companies`
 --
 ALTER TABLE `companies`
-  ADD PRIMARY KEY (`c_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`p_id`),
-  ADD KEY `user` (`u_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`u_id`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `company` (`c_id`);
+  ADD KEY `company` (`company`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -124,19 +145,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `companies`
 --
 ALTER TABLE `companies`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -146,13 +167,13 @@ ALTER TABLE `users`
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`);
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `companies` (`c_id`);
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`company`) REFERENCES `companies` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
